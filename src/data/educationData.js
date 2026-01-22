88fda1d628f8d5be12e6af4625e3ddde3500803a
wendytheelf <you@example.com>
2026-01-22 13:29:53 -0500

    test edit edcation data

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,16 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Marble Hills Middle School',
-        course: 'Higher Secondary Education',
-        startYear: '2007',
-        endYear: '2015'
+        institution: 'Natoinal Taiwan University',
+        course: 'BSW in Social Work',
+        startYear: '2018',
+        endYear: '2023'
     },
     {
         id: 2,
-        institution: 'Somerset School of Science',
-        course: 'Bachelor of Technology',
-        startYear: '2015',
-        endYear: '2019'
-    },
-    {
-        id: 3,
-        institution: 'Hawking University, Cambridge',
-        course: 'Master of Technology',
-        startYear: '2019',
-        endYear: 'Present'
-    },
+        institution: 'Georgetown University',
+        course: 'Master of Data Science & Analytics',
+        startYear: '2024',
+        endYear: '2026'
+    }
 ]
